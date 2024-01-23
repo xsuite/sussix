3b95749e2629b1b97518ed157aa4bb87fff15ce3
Philippe Bélanger <pbelanger@Philippes-Laptop.local>
2024-01-23 10:58:07 +0100

    updated init

--- sussix/__init__.py
+++ sussix/__init__.py
@@ -1,3 +1,3 @@
-from .naff import naff,harmonics,tune,fundamental_frequency
+from .naff import naff,harmonics,tune,fundamental_frequency,_fft_f0_estimate
 from .analysis import find_linear_combinations,generate_signal
 from .windowing import hann
